81b4a6e1eb346460a0793965c6e0470dd6009ace
mafezoli78 <fabricio@mafezoli.com>
2026-02-28 08:39:34 -0300

    Update client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 import { Database } from './types'
 
-const SUPABASE_URL = "https://ynothcoqcdkrfalxfwjj.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "SUA_CHAVE_AQUI";
+const SUPABASE_URL = "https://jhpxfvwhcxakzajioxiz.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "COLE_A_ANON_AQUI";
 
 export const supabase = createClient<Database>(
   SUPABASE_URL,
