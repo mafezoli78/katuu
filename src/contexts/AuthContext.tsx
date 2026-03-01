e95920b9f1628fcd226b3b3749b0033055820978
mafezoli78 <fabricio@mafezoli.com>
2026-03-01 16:13:57 -0300

    Update AuthContext.tsx

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -6,7 +6,7 @@ interface AuthContextType {
   user: User | null;
   session: Session | null;
   loading: boolean;
-  signUp: (email: string, password: string) => Promise<{ error: Error | null }>;
+  signUp: (email: string, password: string, metadata?: { nome: string } ) => Promise<{ error: Error | null }>;
   signIn: (email: string, password: string) => Promise<{ error: Error | null }>;
   signInWithOAuth: (provider: 'google' | 'apple') => Promise<{ error: Error | null }>;
   signOut: () => Promise<void>;
